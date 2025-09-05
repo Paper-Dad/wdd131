38b694ebcee2ac49ebca2e248f17973ebc3b8b77
Paper-Dad <broberts@byupathway.edu>
2025-09-05 17:36:52 -0600

    Completed Home Page Assignment

---
+++ week01/scripts/getdates.js
@@ -0,0 +1,17 @@
+
+const currentYear = new Date().getFullYear();
+
+const yearSpan = document.getElementById("year");
+
+if (yearSpan) {
+  yearSpan.textContent = currentYear;
+}
+
+
+const modified = document.lastModified;
+
+const lastModifiedSpan = document.getElementById("lastModified");
+
+if (lastModifiedSpan) {
+    lastModifiedSpan.textContent = modified;
+}
